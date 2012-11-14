a7761bf54aaeeb21d7965a3d22f606e742fcaa2e
spikebike <bill@broadley.org>
2012-11-14 01:58:27 -0800

    tweaked timers and inserted values

--- supernode.go
+++ supernode.go
@@ -54,14 +54,13 @@ func main() {
 	go dht.DoDHT()
 	go drainresults(dht)
 
-	queryTick := time.Tick(100 * time.Second)
+	queryTick := time.Tick(50 * time.Second)
 
 	for {
 		select {
 		case <-queryTick:
 			// trying the manual method from dht_test in case I have the format 
 			// wrong
-			dht.PeersRequest("\xd1\xc5\x67\x6a\xe7\xac\x98\xe8\xb1\x9f\x63\x56\x59\x05\x10\x5e\x3c\x4c\x37\xa2", true)
 			fmt.Printf("TICK\n")
 			for i := 0; i < num; i++ {
 				l4g.Info("querying for infoHash: %x", shalist[i])
